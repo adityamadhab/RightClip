00af7dfb88322fc46262b63ebb515ef44d137eb4
adityamadhab <adityamadhabborah@gmail.com>
2024-06-22 22:40:16 +0530

    Commit - 22.06.24

--- frontend/src/components/Creator-Components/CreatorNav.jsx
+++ frontend/src/components/Creator-Components/CreatorNav.jsx
@@ -1,13 +1,13 @@
+import { Link } from 'react-router-dom';
 
 function CreatorNavbar() {
-
   return (
     <nav className="bg-white shadow-md border-b-2 border-gray-300 h-[60px]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <div className="flex-shrink-0">
+          <Link to={'/'} className="flex-shrink-0 cursor-pointer">
             <img className="h-8 w-auto" src="/Creator-assests/logo2.png" alt="Logo" />
-          </div>
+          </Link>
         </div>
       </div>
     </nav>
